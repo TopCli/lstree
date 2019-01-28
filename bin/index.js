4c23c31819e278ab6628714fda6a465e21af8c7b
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-01-28 20:47:10 +0100

    feat: add --ignore to bin

--- bin/index.js
+++ bin/index.js
@@ -8,10 +8,12 @@ const lstree = require("../index");
 
 const argParsed = parseArg([
     argDefinition("-d --depth [number=2]", "Limit the tree depth display. root is equal to 0"),
-    argDefinition("-s --showfd", "Display files description to their right")
+    argDefinition("-s --showfd", "Display files description to their right"),
+    argDefinition("-i --ignore [array]", "List of files to ignore")
 ]);
 
 lstree({
     depth: argParsed.get("depth"),
-    showFilesDescriptor: argParsed.get("showfd")
+    showFilesDescriptor: argParsed.get("showfd"),
+    ignore: argParsed.get("ignore")
 })(process.cwd());
