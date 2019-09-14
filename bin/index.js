54a9cb4b3a2c05b0443e7fa049e53be48067d066
Croquet Mickael <mickael.croquet@laposte.net>
2019-09-14 22:25:23 +0200

    chore: replacing @slimio/arg-parser by lukeed/sade

--- bin/index.js
+++ bin/index.js
@@ -3,19 +3,24 @@
 "use strict";
 
 // Require Third-party Dependencies
-const { parseArg, argDefinition } = require("@slimio/arg-parser");
+const sade = require("sade");
 
 // Require internal dependencies
 const lstree = require("../index");
+const prog = sade("", true)
+    .option("-d --depth [number=0]", "Limit the tree depth display. root is equal to 0", 0)
+    .option("-s --showfd", "Display files description to their right", false)
+    .option("-i --ignore [array]", "List of files to ignore", [])
+    .action((opts) => {
+        const ignore = typeof opts.ignore === "string" ? opts.ignore.split(",") : [];
+        const depth = typeof opts.depth === "boolean" ? 0 : Number(opts.depth);
+        console.log(ignore);
+        console.log(depth);
+        lstree({
+            depth: Number.isNaN(depth) ? 0 : depth,
+            showFilesDescriptor: Boolean(opts.showfd),
+            ignore
+        })(process.cwd())
+    });
 
-const argParsed = parseArg([
-    argDefinition("-d --depth [number=0]", "Limit the tree depth display. root is equal to 0"),
-    argDefinition("-s --showfd", "Display files description to their right"),
-    argDefinition("-i --ignore [array]", "List of files to ignore")
-]);
-
-lstree({
-    depth: argParsed.get("depth"),
-    showFilesDescriptor: argParsed.get("showfd"),
-    ignore: argParsed.get("ignore")
-})(process.cwd());
+prog.parse(process.argv);
