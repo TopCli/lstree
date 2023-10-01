035dff5940e314da7c585972ed50353748e00395
PierreDemailly <pierredemailly.pro@gmail.com>
2023-10-01 12:48:57 +0200

    chore: fix test, add some options

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,15 @@
-declare namespace lstree {
-  interface options {
-    ignore?: string[];
-    description?: Map<string, string>;
-    depth?: number;
-    showFilesDescriptor?: boolean;
-  }
-
-  export function tree(options?: options): (dir: string, pRootPath?: number) => void;
+export interface LSTreeOptions {
+  ignore?: string[];
+  description?: Map<string, string>;
+  depth?: number;
+  showFilesDescriptor?: boolean;
+  showTitle?: boolean;
+  title?: string;
+  margin?: {
+    top?: number;
+    left?: number;
+    bottom?: number;
+  };
 }
-
-export as namespace lstree;
-export = lstree;
+  
+export default function tree(options?: LSTreeOptions): (dir: string, pRootPath?: number) => Promise<void>;
