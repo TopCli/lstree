c5622dab9416af158de4502a051fbdc3b41a0e44
PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
2023-10-09 20:59:00 +0200

    chore: fix tests, add some options (#127)
    * chore: fix test, add some options
    
    * chore: update README

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 // Import Node.js Dependencies
 import fs from "fs/promises";
 import path from "path";
+import os from "node:os";
 
 // Import Third-party Dependencies
 import kleur from "kleur";
 import is from "@slimio/is";
 
-// CONSTANTS
 const { yellow, white, cyan, gray } = kleur;
+
 /**
- * @version 0.1.0
- * @function tree
- * @description desc clojure
- * @memberof lstree
  * @param {object} [options] object representing the options for customizing the tree view.
  * @param {string[]} [options.ignore] allows you to exclude files or folders from the tree.
  * @param {Map} [options.description] allows you to add a description for files to their right.
+ * The key is the name of file, value is the description of the file.
  * @param {number} [options.depth=1] Wanted depth
  * @param {boolean} [options.showFilesDescriptor=false] view file description
- * The key is the name of file, value is the description of the file.
- * @returns {Promise}
+ * @param {boolean} [options.showTitle=true] view lstree title
+ * @param {string} [options.title="project tree"] allow to add a custom title, default "project tree"
+ * @param {number} [options.margin.top] allow to add a margin top (one new line per number)
+ * @param {number} [options.margin.left] allow to add a margin left (two spaces per number)
+ * @param {number} [options.margin.bottom] allow to add a margin bottom (one new line per number)
+ * @returns {(path: string) => Promise<void>}
  *
  * @example
  * const options = {
@@ -34,17 +36,56 @@ const { yellow, white, cyan, gray } = kleur;
 export default function tree(options = Object.create(null)) {
   const IGNORE_FILE = new Set(["node_modules", "coverage", "docs", ".nyc_output", ".git"]);
   const DESC_FILE = new Map([
-    [".eslintrc", "ESLint configuration"],
-    [".editorconfig", "Configuration for the code editor"],
-    [".gitignore", "Files to ignore on GIT"],
-    [".npmignore", "Files to ignore when publishing the NPM package"],
-    [".npmrc", "Local configuration of NPM"],
-    ["LICENCE", "Legal license of the project"],
-    ["CONTRIBUTING.md", "Code of Conduct & Contribution Rules for the SlimIO Project"],
-    ["commitlint.config.json", "Configuration of the convention to respect for GIT commits"],
-    ["jsdoc.json", "Configuration to generate the JSDoc with the command npm run doc"],
+  // Configuration files
+    [".babelrc", "Babel configuration for JavaScript transpilation"],
+    ["tsconfig.json", "TypeScript configuration for TypeScript projects"],
+    ["jest.config.js", "Jest configuration for testing"],
+    ["webpack.config.js", "Webpack configuration for bundling assets"],
+    [".all-contributorsrc", "All Contributors configuration for managing project contributors"],
+    [".editorconfig", "EditorConfig configuration for maintaining consistent coding styles between different editors and IDEs"],
+    [".gitignore", " Specifies files and directories to be ignored by Git"],
+    [".npmrc", "Configuration file for npm"],
+
+    // Dependency management
+    ["package-lock.json", "npm package lock file for managing package dependencies with npm"],
+    ["yarn.lock", "Yarn lock file for managing package dependencies with Yarn"],
+    ["pnpm-lock.yaml", "pnpm lock file for managing package dependencies with pnpm"],
+
+    // Scripts and automation
+    ["prettier.config.js", "Prettier configuration for code formatting"],
+    ["husky.config.js", "Husky configuration for Git hooks"],
+    ["lint-staged.config.js", "Lint-staged configuration for running linters on staged files"],
+
+    // Documentation
+    ["CHANGELOG.md", "Changelog to document project changes and version history"],
+    ["CODE_OF_CONDUCT.md", "Code of Conduct for the project contributors"],
+    ["CONTRIBUTING.md", "Guidelines for contributing to the project"],
+
+    // Testing
+    [".env.example", "Example environment variables file for local development"],
+
+    // Continuous Integration
+    [".github", "GitHub Actions configuration for automating workflows"],
+    ["dependabot.yml", "Dependabot configuration for automating dependency updates"],
+    ["scorecard.yml", "OSSF Scorecard analysis configuration for evaluating open source projects"],
+    ["codeql.yml", "CodeQL configuration for analyzing code quality"],
+    ["node.js.yml", "Node.js pipeline configuration for running tests, linting..."],
+
+    // Docker
+    ["Dockerfile", "Docker configuration for containerizing the application"],
+    ["docker-compose.yml", "Docker Compose configuration for managing containers"],
+
+    // Environment Variables
+    [".env", "Environment variables file for local development (contains sensitive data, not committed to VCS)"],
+
+    // Node.js runtime
+    ["index.js", "Main application file"],
+
+    // Other project-related files
+    ["LICENSE", "Legal license of the project"],
+    ["jsdoc.json", "Configuration to generate JSDoc with the command npm run doc"],
     ["package.json", "Manifest of the project"],
-    ["README.md", "Documentation of the projet (start, use...)"]
+    ["README.md", "Documentation of the project (start, use...)"]
   ]);
 
   // Retrieve and apply arguments
@@ -71,7 +112,7 @@ export default function tree(options = Object.create(null)) {
    * @memberof lstree
    * @param {!string} dir directory path. Path can handle "/" and "\" separator and not end with a separator
    * @param {number} [pRootPath] path of the root folder if there is recursivity
-   * @returns {void}
+   * @returns {Promise<void>}
    * @example
    * tree("C:/path/to/your/directory/newProject");
    * output expected :
@@ -96,21 +137,19 @@ export default function tree(options = Object.create(null)) {
     if (is.nullOrUndefined(dir)) {
       throw new Error("Current working directory path is missing");
     }
-    if (dir.match(/\//gi)) {
-      // eslint-disable-next-line
-      dir = dir.replace(/\//gi, "\\");
-    }
-    if (dir.match(/\\$/gi)) {
-      // eslint-disable-next-line
-      dir = dir.replace(/\\$/gi, "");
+
+    let marginTop = options.margin?.top;
+    if (pRootPath === null && marginTop) {
+      while (marginTop-- > 0) {
+        console.log("");
+      }
     }
 
     const rootPath = pRootPath === null ? dir : pRootPath;
     // Calculate Depth with root folder and number of separators "\"
-    const depth = is.nullOrUndefined(dir.replace(rootPath, "").match(/\\\w+/g)) ?
-      0 : dir.replace(rootPath, "").match(/\\\w+/g).length;
+    const depth = dir.replace(rootPath, "").match(/[/\\]/g, "")?.length ?? 0;
 
-    let strAddDepth = "";
+    let strAddDepth = "".padStart(2 * options.margin?.left ?? 0, " ");
     if (depth > 0) {
       for (let index = 0; index < depth; index++) {
         strAddDepth += yellow("│ ");
@@ -126,8 +165,9 @@ export default function tree(options = Object.create(null)) {
     let nbFolder = 0;
 
     // Print only one time at the begginning
-    if (depth === 0) {
-      console.log(gray("\n > project tree\n"));
+    if (depth === 0 && (is.bool(options.showTitle) ? options.showTitle : true)) {
+      const title = options.title ?? "project tree";
+      console.log(gray(`${os.EOL} > ${title}${os.EOL}`));
     }
 
     // eslint-disable-next-line
@@ -156,16 +196,35 @@ export default function tree(options = Object.create(null)) {
       }
     }
 
-    const last = files.length - 1;
+    function getPrefix(index) {
+      if (nbFolder === 0 && depth === 0 && files.length === 1) {
+        return "──";
+      }
+
+      if (index === files.length - 1) {
+        return "└─";
+      }
+
+      return nbFolder === 0 && depth === 0 && index === 0 ? "┌─" : "├─";
+    }
+
     // Print all files after folders
-    for (const [ind, val] of files.entries()) {
-      if (viewFileDescription && DESC_FILE.has(val)) {
+    for (const [index, fileName] of files.entries()) {
+      const prefix = getPrefix(index);
+      if (viewFileDescription && DESC_FILE.has(fileName)) {
         // ajouter la desc a la droite
-        const desc = DESC_FILE.get(val);
-        console.log(yellow(`${strAddDepth}${ind === last ? "└" : "├"} ${white(`${val}`)} ${cyan(`(${desc})`)}`));
+        const desc = DESC_FILE.get(fileName);
+        console.log(yellow(`${strAddDepth}${prefix} ${white(`${fileName}`)} ${cyan(`(${desc})`)}`));
       }
       else {
-        console.log(yellow(`${strAddDepth}${ind === last ? "└" : "├"} ${white(`${val}`)}`));
+        console.log(yellow(`${strAddDepth}${prefix} ${white(`${fileName}`)}`));
+      }
+    }
+
+    let marginBottom = options.margin?.bottom;
+    if (depth === 0 && options.margin?.bottom) {
+      while (marginBottom-- > 0) {
+        console.log("");
       }
     }
   };
--- test/ut/test.js
+++ test/ut/test.js
@@ -1,8 +1,10 @@
 // Import Node.js Dependencies
 import assert from "node:assert";
 import { test } from "node:test";
-import { EOL } from "node:os";
 import { join } from "node:path";
+
+// Import Third-party Dependencies
+import stripAnsi from "strip-ansi";
 
 // Import Internal Dependencies
 import tree from "../../index.js";
@@ -16,15 +18,52 @@ test("it should throw an error when directory path is missing", async() => {
   }
 });
 
-// TODO: package is completely broken
-// test("it should print the list of the entire folder and file in the directory", async() => {
-//   const logs = [];
-//   console.log = (data) => logs.push(data);
+test("it should print the list of the entire folder and file in the directory", async() => {
+  const logs = [];
+  console.log = (data) => logs.push(stripAnsi(data).trim());
 
-//   await tree({ depth: 3 })(join(process.cwd(), "/test/fixtures"));
-//   assert.equal(logs.shift(), "┌─📁 folderA");
-//   assert.equal(logs.shift(), "│ ├─📁 folderB");
-//   assert.equal(logs.shift(), "│ │ └─📄 fileB.js");
-//   assert.equal(logs.shift(), "│ └─📄 fileA.txt");
-//   assert.equal(logs.shift(), "└─📄 index.js");
-// });
+  await tree({ depth: 3 })(join(process.cwd(), "/test/fixtures"));
+  assert.equal(logs.shift(), "> project tree");
+  assert.equal(logs.shift(), "┌─📁 folderA");
+  assert.equal(logs.shift(), "│ ├─📁 folderB");
+  assert.equal(logs.shift(), "│ │ └─ b.js");
+  assert.equal(logs.shift(), "│ └─ fileA.txt");
+  assert.equal(logs.shift(), "└─ index.js");
+});
+
+test("it should add and show custom description", async() => {
+  const logs = [];
+  console.log = (data) => logs.push(stripAnsi(data).trim());
+
+  await tree({
+    depth: 3,
+    description: new Map([["fileA.txt", "foo"]]),
+    showFilesDescriptor: true
+  })(join(process.cwd(), "/test/fixtures"));
+
+  assert.equal(logs.shift(), "> project tree");
+  assert.equal(logs.shift(), "┌─📁 folderA");
+  assert.equal(logs.shift(), "│ ├─📁 folderB");
+  assert.equal(logs.shift(), "│ │ └─ b.js");
+  assert.equal(logs.shift(), "│ └─ fileA.txt (foo)");
+  assert.equal(logs.shift(), "└─ index.js (Main application file)");
+});
+
+test("it should ignore folder", async() => {
+  const logs = [];
+  console.log = (data) => logs.push(stripAnsi(data).trim());
+
+  await tree({ depth: 3, ignore: ["folderA"] })(join(process.cwd(), "/test/fixtures"));
+  assert.equal(logs.shift(), "> project tree");
+  assert.equal(logs.shift(), "── index.js");
+});
+
+test("it show custom title", async() => {
+  const logs = [];
+  console.log = (data) => logs.push(stripAnsi(data).trim());
+
+  await tree({ depth: 3, ignore: ["folderA"], title: "foo" })(join(process.cwd(), "/test/fixtures"));
+  assert.equal(logs.shift(), "> foo");
+  assert.equal(logs.shift(), "── index.js");
+});
+
